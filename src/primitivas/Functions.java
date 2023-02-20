5abfc0657990d82e42bbd22211fb1cd893672d40
carri <isabella.carrillo@correo.unimet.edu.ve>
2023-02-20 14:58:51 -0400

    commit

--- src/primitivas/Functions.java
+++ src/primitivas/Functions.java
@@ -4,10 +4,33 @@
  */
 package primitivas;
 
+import java.io.PrintWriter;
+import javax.swing.JOptionPane;
+import grafos.GrafoMA;
+
 /**
  *
  * @author carri
  */
 public class Functions {
     
+    public void write_txt (){
+        try {
+            PrintWriter pw = new PrintWriter("C:\\Users\\carri\\Documents\\NetBeansProjects\\Proyecto Amazon\\src\\primitivas\\amazon.txt");
+            pw.write("");
+            
+            JOptionPane.showMessageDialog(null,"Info succesfully loaded!");
+            
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null, "There's been an error");
+        }
+    }
+    
+    public void addWarehouse (){
+        
+    }
+    
 }
+
+
+
